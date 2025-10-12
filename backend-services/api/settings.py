aab0b90bef4da229d2b78e19579b46d343efdf99
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-10-12 14:06:23 +0100

    fix(backend-services): add static files configuration and update vercel settings

--- backend-services/api/settings.py
+++ backend-services/api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = "static/"
 
