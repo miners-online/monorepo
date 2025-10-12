e076ed6fe13afa5f9ad1ba371bb1724d59293554
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-10-12 17:20:10 +0100

    fix(backend-services): update Python version in build_files.sh to 3.12

--- backend-services/build_files.sh
+++ backend-services/build_files.sh
@@ -1,4 +1,4 @@
-python3.9 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput
